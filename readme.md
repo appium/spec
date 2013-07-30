b7c66c839f7a520ea6eb9e22f54ecf493c7dab41
bootstraponline <code@bootstraponline.com>
2013-07-29 20:02:25 -0400

    Fix test

--- readme.md
+++ readme.md
@@ -1,7 +1,6 @@
 #### spec [![Gem Version](https://badge.fury.io/rb/spec.png)](http://rubygems.org/gems/spec)[![Dependency Status](https://gemnasium.com/bootstraponline/spec.png)](https://gemnasium.com/bootstraponline/spec)
 
-A modified minitest for Appium.
+A modified [MiniTest](https://github.com/seattlerb/minitest) for Appium.
 
-
-- `rake b_install` - Uninstall old spc, build spec, install new spec.
+- `rake b_install` - Uninstall old gem, build gem, install new gem.
 - `rake rel` - Release new `spec` to GitHub and RubyGems.
---
+++ test/manual/run2.rb
@@ -0,0 +1,75 @@
+# encoding: utf-8
+
+require 'rubygems'
+require 'stringio' # stdlib
+require 'spec' # not 'minitest'
+require 'minitest/spec'
+
+class MiniTest::Spec
+  @@output = StringIO.new
+  
+  def self.output
+    @@output
+  end
+  
+  def p msg
+    @@output.puts msg
+  end
+end
+
+class SpecTest < MiniTest::Spec
+  describe 'a' do
+    t('') { p 1 }
+    t('') { p 2 }
+    t('') { p 3 }
+    t('') { p 4 }
+    t('') { p 5 }
+    t('') { p 6 }
+    t('') { p 7 }
+  end
+
+  describe 'b' do
+    before { p '-- before' }
+    t('z') { p 8 }
+    t('b') { p 9 }
+    t('a') { p 10 }
+  end
+
+  describe 'z' do
+    t('a') { p 11 }
+    t('z') { p 12 }
+  end
+end
+
+options = {}
+out = MiniTest::Spec.output
+reporter = Minitest::CompositeReporter.new
+reporter << Minitest::SummaryReporter.new(out, options)
+reporter << Minitest::ProgressReporter.new(out, options)
+
+Minitest.__run reporter, options
+
+out.flush
+out.rewind
+
+actual = out.readlines.join ''
+exp = <<'EXP'.strip
+1
+.2
+.3
+.4
+.5
+.6
+.7
+.-- before
+8
+.-- before
+9
+.-- before
+10
+.11
+.12
+.
+EXP
+
+puts actual.must_equal exp
