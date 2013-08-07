6f52eedbc2b6a2f963af31b8a9abc8be81b327e7
bootstraponline <code@bootstraponline.com>
2013-08-07 14:40:56 -0400

    Fix trace

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -148,9 +148,12 @@ module Minitest
 
     targets = []
     files = {}
+    last_file = ''
+    last_line = -1
 
     files_to_trace = spec_opts.fetch(:trace, []);
     io = spec_opts.fetch(:io, $stdout)
+    color = spec_opts.fetch(:color, "\e[32m") # ANSI.green default
     # target only existing readable files
     files_to_trace.each { |f| targets.push(f) if File.exists?(f) && File
     .readable?(f) }
@@ -159,12 +162,21 @@ module Minitest
     set_trace_func(lambda do |event, file, line, id, binding, classname|
       return unless targets.include? file
 
+      # never repeat a line
+      return if file == last_file && line == last_line
+
       file_sym = file.intern
       files[file_sym] = IO.readlines(file) if files[file_sym].nil?
       lines = files[file_sym]
 
       # arrays are 0 indexed and line numbers start at one.
+      io.print color if color # ANSI code
       io.puts lines[ line - 1]
+      io.print "\e[0m" if color # ANSI.clear
+
+      last_file = file
+      last_line = line
+
     end)
   end
 
