a95dafa6bea7b7dbdfb301aece3b04bd7a6c9bd4
bootstraponline <code@bootstraponline.com>
2013-08-21 10:06:58 -0400

    Add skip test 2

---
+++ test/manual/skip2.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+require 'rubygems'
+require 'spec'
+require 'minitest/autorun'
+
+describe 'a' do
+  it('') { puts 1 }
+  it('') { skip }
+  it('') { puts 3 }
+  it('') { skip }
+  it('') { puts 5 }
+end
