87ac203d0d80902fd99344b425c1aa6f2d307a87
bootstraponline <code@bootstraponline.com>
2014-04-21 16:12:55 -0400

    Add run once example

---
+++ test/manual/run_once_in_before.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'spec'
+
+describe 'test' do
+  def run_once
+    puts 'run once!'
+    self.class.send :define_method, :run_once, proc {}
+  end
+
+  before { run_once }
+
+  t '1' do
+  end
+
+  t '2' do
+  end
+
+  t '3' do
+  end
+end
+
+trace_files = [File.expand_path(__FILE__)]
+Minitest.run_specs({trace: trace_files})
